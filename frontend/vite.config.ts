20036255b83801a9eaedac037041785c9d1cda34
邱振源 <yuanciou@qiuzhenyuandeMacBook-Air.local>
2025-09-20 23:58:23 +0800

    fix: change to tcp4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,8 +9,9 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/mch-2025-cloudmosa/' : '/',
   server: {
-    host: '0.0.0.0', // 監聽所有網絡接口
-    port: 5173,      // 可選：指定端口
+    host: '0.0.0.0',   // 監聽所有網絡接口
+    port: 5173,        // 指定端口
+    strictPort: true,  // 如果端口被佔用則失敗
   },
   plugins: [
     vue(),
