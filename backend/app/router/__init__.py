901102155e4ee709962d98a50f032cf9473d8d96
Leafoliage <leafoliages@gmail.com>
2025-09-20 20:26:59 +0800

    Add Job router

--- backend/app/router/__init__.py
+++ backend/app/router/__init__.py
@@ -5,5 +5,6 @@ from .items import router as items_router
 from .files import router as files_router
 from .auth import router as auth_router
 from .profile import router as profile_router
+from .jobs import router as job_router
 
-__all__ = ["health_router", "items_router", "auth_router", "profile_router", "files_router"]
+__all__ = ["health_router", "items_router", "auth_router", "profile_router", "files_router", "job_router"]
