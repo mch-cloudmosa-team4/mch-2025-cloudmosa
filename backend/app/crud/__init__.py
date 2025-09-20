b6a98bcf140f86bad16c418933c506880f83316f
Sean20405 <sean20405.cs11@nycu.edu.tw>
2025-09-20 22:55:47 +0800

    add: skill CRUD

--- backend/app/crud/__init__.py
+++ backend/app/crud/__init__.py
@@ -4,5 +4,6 @@ from .item import item
 from .user import user
 from .profile import profile
 from .jobs import jobs
+from .skills import skills
 
-__all__ = ["item", "user", "profile", "jobs"]
+__all__ = ["item", "user", "profile", "jobs", "skills"]
